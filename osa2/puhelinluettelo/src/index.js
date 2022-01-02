d2b4dd972155bc6c43a0ef4dc36a5c020e0adf8c
Lauri Viitanen <lauri.viitanen@tuni.fi>
2022-01-02 19:48:18 +0200

    2.11 tehty

--- osa2/puhelinluettelo/src/index.js
+++ osa2/puhelinluettelo/src/index.js
@@ -1,18 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
+import axios from 'axios'
 import InputForm from './components/inputForm'
 import PhonebookList from './components/phonebookList'
 import FilterForm from './components/filterForm'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [ persons, setPersons ] = useState([])
   const [ filterInput, setFilterInput ] = useState('')
   
+  useEffect(() => {
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      setPersons(response.data)
+    })
+  }, [])
+
   const addPerson = (newPerson, clearInputForms) => {
     if (personExistsAllready(newPerson)) {
       alert(`${newPerson.name} is already added to phonebook`)
